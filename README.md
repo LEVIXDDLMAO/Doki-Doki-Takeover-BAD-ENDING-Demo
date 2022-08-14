4093acd1ccdcabab64de6da38871cb24dc165941
MemeHoovy <isaiahryan1200@gmail.com>
2022-08-14 02:17:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+![Doki Doki Takeover! - BAD ENDING logo](art/api.png)
+## Built on **[Psych Engine](https://github.com/ShadowMario/FNF-PsychEngine)** - **[GameBanana](https://gamebanana.com/mods/47364)** - **[Game Jolt](https://gamejolt.com/games/dokidoki/654415)**
+
+# Original Game Links
+
+FNF Links: **[itch.io page](https://ninja-muffin24.itch.io/funkin) ⋅ [Newgrounds](https://www.newgrounds.com/portal/view/770371) ⋅ [source code on GitHub](https://github.com/ninjamuffin99/Funkin)**
+
+DDLC Links: **[DDLC Official Website](http://ddlc.moe) ⋅ [itch.io page](https://teamsalvato.itch.io/ddlc) ⋅ [Steam page](https://store.steampowered.com/app/698780/Doki_Doki_Literature_Club)**
+
+DDLC+ Links: **[DDLC+ Official Website](http://ddlc.plus) ⋅ [Steam page](https://store.steampowered.com/app/1388880/Doki_Doki_Literature_Club_Plus)**
+
+# Credits
+
+### Doki Doki Takeover
+- [Jorge_SunSpirit](https://twitter.com/Jorge_SunSpirit) and [M&M](https://twitter.com/ActualMandM) - Programming
+- [SirDusterBuster](https://twitter.com/SirDusterBuster) and [TokyoGalaxyOG](https://twitter.com/TokyoGalaxyOG) - Art
+- [Matt$](https://twitter.com/matt_currency) and [Kallionic](https://youtube.com/kallionic) - Music
+- [Esther Christo, voice of Monika](https://twitter.com/carimellevo)
+- [Miss Beepy, voice of Natsuki](https://twitter.com/MissBeepy)
+- [Loganne Digma, voice of Yuri](https://twitter.com/Loganne_Digma)
+- [Phebe Fabacher, voice of Sayori](https://twitter.com/HarudoriVA)
+
+### Friday Night Funkin'
+ - [ninjamuffin99](https://twitter.com/ninja_muffin99) - Programming
+ - [PhantomArcade3K](https://twitter.com/phantomarcade3k) and [Evilsk8r](https://twitter.com/evilsk8r) - Art
+ - [Kawai Sprite](https://twitter.com/kawaisprite) - Music
+
+### Doki Doki Literature Club!
+- [Dan Salvato](https://twitter.com/dansalvato) - Creator of DDLC
+- [Team Salvato](https://twitter.com/TeamSalvato) - Publishers of DDLC
+
+### Psych Engine
+- [ShadowMario](https://twitter.com/Shadow_Mario_) - Maintainer and lead coder
+- [RiverOaken](https://twitter.com/river_oaken) - Art and animations
+- [shubs](https://twitter.com/yoshubs) - Assistant coder
+- [The contributors](https://github.com/ShadowMario/FNF-PsychEngine/graphs/contributors)
---
+++ source/MainMenuState.hx
@@ -0,0 +1,352 @@
+package;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.effects.FlxFlicker;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import lime.app.Application;
+import Achievements;
+import editors.MasterEditorMenu;
+import flixel.input.keyboard.FlxKey;
+import flixel.group.FlxGroup;
+import flixel.addons.display.FlxBackdrop;
+import flixel.util.FlxTimer;
+
+using StringTools;
+
+class MainMenuState extends MusicBeatState
+{
+	public static var psychEngineVersion:String = '0.5.1'; // This is also used for Discord RPC
+	public static var curSelected:Int = 0;
+
+	var menuItems:FlxTypedGroup<FlxSprite>;
+	private var camGame:FlxCamera;
+	private var camAchievement:FlxCamera;
+
+	var optionShit:Array<String> = [
+		'story mode',
+		'freeplay',
+		#if ACHIEVEMENTS_ALLOWED 'awards',
+		#end
+		'credits',
+		'options'
+	];
+
+	public static var firstStart:Bool = true;
+
+	var logo:FlxSprite;
+	var logoBl:FlxSprite;
+
+	var backdrop:FlxBackdrop;
+
+	var debugKeys:Array<FlxKey>;
+
+	override function create()
+	{
+		#if STREAMER_DEMO
+		optionShit.remove('story mode');
+		#end
+
+		if (!FlxG.sound.music.playing)
+			FlxG.sound.playMusic(Paths.music('freakyMenu'));
+
+		#if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the Menus", null);
+		#end
+
+		debugKeys = ClientPrefs.copyKey(ClientPrefs.keyBinds.get('debug_1'));
+
+		transIn = FlxTransitionableState.defaultTransIn;
+		transOut = FlxTransitionableState.defaultTransOut;
+
+		backdrop = new FlxBackdrop(Paths.image('scrolling_BG'));
+		backdrop.velocity.set(-40, -40);
+		backdrop.antialiasing = ClientPrefs.globalAntialiasing;
+		add(backdrop);
+
+		logo = new FlxSprite(-260, 0).loadGraphic(Paths.image('Credits_LeftSide'));
+		logo.antialiasing = ClientPrefs.globalAntialiasing;
+		add(logo);
+		if (firstStart)
+			FlxTween.tween(logo, {x: -60}, 1.2, {
+				ease: FlxEase.elasticOut,
+				onComplete: function(flxTween:FlxTween)
+				{
+					firstStart = false;
+					changeItem();
+				}
+			});
+		else
+			logo.x = -60;
+
+		logoBl = new FlxSprite(-160, -40);
+		logoBl.frames = Paths.getSparrowAtlas('logoBumpin');
+		logoBl.antialiasing = ClientPrefs.globalAntialiasing;
+		logoBl.scale.set(0.5, 0.5);
+		logoBl.animation.addByPrefix('bump', 'logo bumpin', 24, false);
+		logoBl.animation.play('bump');
+		logoBl.updateHitbox();
+		add(logoBl);
+		if (firstStart)
+			FlxTween.tween(logoBl, {x: 40}, 1.2, {
+				ease: FlxEase.elasticOut,
+				onComplete: function(flxTween:FlxTween)
+				{
+					firstStart = false;
+					changeItem();
+				}
+			});
+		else
+			logoBl.x = 40;
+
+		menuItems = new FlxTypedGroup<FlxSprite>();
+		add(menuItems);
+
+		var tex = Paths.getSparrowAtlas('credits_assets');
+
+		for (i in 0...optionShit.length)
+		{
+			var menuItem:FlxSprite = new FlxSprite(-350, 370 + (i * 50));
+			menuItem.frames = tex;
+			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
+			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
+			menuItem.animation.play('idle');
+			menuItem.ID = i;
+			menuItem.scale.set(1.5, 1.5);
+			// menuItem.screenCenter(X);
+			menuItems.add(menuItem);
+			menuItem.scrollFactor.set();
+			menuItem.antialiasing = ClientPrefs.globalAntialiasing;
+			if (firstStart)
+				FlxTween.tween(menuItem, {x: 50}, 1.2 + (i * 0.2), {
+					ease: FlxEase.elasticOut,
+					onComplete: function(flxTween:FlxTween)
+					{
+						firstStart = false;
+						changeItem();
+					}
+				});
+			else
+				menuItem.x = 50;
+		}
+
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "DDTO BAD ENDING v" + Application.current.meta.get('version'), 12);
+		#if STREAMER_DEMO
+		versionShit.text = "DDTO BAD ENDING (STREAMER DEMO)";
+		#end
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);
+
+		// NG.core.calls.event.logEvent('swag').send();
+
+		changeItem();
+
+		#if ACHIEVEMENTS_ALLOWED
+		Achievements.loadAchievements();
+		var leDate = Date.now();
+		if (leDate.getDay() == 5 && leDate.getHours() >= 18)
+		{
+			var achieveID:Int = Achievements.getAchievementIndex('friday_night_play');
+			if (!Achievements.isAchievementUnlocked(Achievements.achievementsStuff[achieveID][2]))
+			{ // It's a friday night. WEEEEEEEEEEEEEEEEEE
+				Achievements.achievementsMap.set(Achievements.achievementsStuff[achieveID][2], true);
+				giveAchievement();
+				ClientPrefs.saveSettings();
+			}
+		}
+		#end
+
+		super.create();
+	}
+
+	#if ACHIEVEMENTS_ALLOWED
+	// Unlocks "Freaky on a Friday Night" achievement
+	function giveAchievement()
+	{
+		add(new AchievementObject('friday_night_play', camAchievement));
+		FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+		trace('Giving achievement "friday_night_play"');
+	}
+	#end
+
+	var selectedSomethin:Bool = false;
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music.volume < 0.8)
+		{
+			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+		}
+
+		if (FlxG.keys.justPressed.F)
+			FlxG.fullscreen = !FlxG.fullscreen;
+
+		if (!selectedSomethin)
+		{
+			var ctrl = FlxG.keys.justPressed.CONTROL;
+
+			if (controls.UI_UP_P)
+			{
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+				changeItem(-1);
+			}
+
+			if (controls.UI_DOWN_P)
+			{
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+				changeItem(1);
+			}
+
+			#if debug
+			if (FlxG.keys.pressed.I)
+				MusicBeatState.switchState(new DemoThanksState());
+			#end
+
+			
+			if (ctrl && curSelected == 0)
+			{
+				openSubState(new GameplayChangersSubstate());
+			}
+
+			if (controls.ACCEPT)
+			{
+				selectedSomethin = true;
+				FlxG.sound.play(Paths.sound('confirmMenu'));
+
+				menuItems.forEach(function(spr:FlxSprite)
+				{
+					if (curSelected != spr.ID)
+					{
+						FlxTween.tween(spr, {alpha: 0}, 1.3, {
+							ease: FlxEase.quadOut,
+							onComplete: function(twn:FlxTween)
+							{
+								spr.kill();
+							}
+						});
+					}
+					else
+					{
+						if (ClientPrefs.flashing)
+						{
+							FlxFlicker.flicker(spr, 1, 0.06, false, false, function(flick:FlxFlicker)
+							{
+								goToState();
+							});
+						}
+						else
+						{
+							new FlxTimer().start(1, function(tmr:FlxTimer)
+							{
+								goToState();
+							});
+						}
+					}
+				});
+			}
+			#if desktop
+			else if (FlxG.keys.anyJustPressed(debugKeys))
+			{
+				selectedSomethin = true;
+				MusicBeatState.switchState(new MasterEditorMenu());
+			}
+			#end
+		}
+
+		if (FlxG.sound.music != null)
+			Conductor.songPosition = FlxG.sound.music.time;
+
+		super.update(elapsed);
+	}
+
+	function goToState()
+	{
+		var daChoice:String = optionShit[curSelected];
+
+		switch (daChoice)
+		{
+			case 'story_mode' | 'story mode':
+				#if STREAMER_DEMO
+				PlayState.storyPlaylist = ['STAGNANT'];
+				#else
+				PlayState.storyPlaylist = ['STAGNANT', 'MARKOV', 'HOME'];
+				#end
+				PlayState.isStoryMode = true;
+				PlayState.storyDifficulty = 2;
+				PlayState.SONG = Song.loadFromJson(PlayState.storyPlaylist[0].toLowerCase() + '-hard', PlayState.storyPlaylist[0].toLowerCase());
+				PlayState.campaignScore = 0;
+				PlayState.campaignMisses = 0;
+				LoadingState.loadAndSwitchState(new PlayState(), true);
+				FreeplayState.destroyFreeplayVocals();
+			case 'freeplay':
+				MusicBeatState.switchState(new FreeplayState());
+			#if MODS_ALLOWED
+			case 'mods':
+				MusicBeatState.switchState(new ModsMenuState());
+			#end
+			case 'awards':
+				MusicBeatState.switchState(new AchievementsMenuState());
+			case 'credits':
+				if (FlxG.keys.pressed.G)
+				{
+					CoolUtil.browserLoad('https://www.youtube.com/watch?v=0MW9Nrg_kZU');
+					FlxG.resetState();
+				}
+				else
+					MusicBeatState.switchState(new CreditsState());
+			case 'options':
+				#if MODS_ALLOWED
+				if (FlxG.keys.pressed.M)
+					MusicBeatState.switchState(new ModsMenuState());
+				else
+				#end
+				MusicBeatState.switchState(new options.OptionsState());
+		}
+	}
+
+	function changeItem(huh:Int = 0)
+	{
+		curSelected += huh;
+
+		if (curSelected >= optionShit.length)
+			curSelected = 0;
+		if (curSelected < 0)
+			curSelected = optionShit.length - 1;
+
+		menuItems.forEach(function(spr:FlxSprite)
+		{
+			spr.animation.play('idle');
+
+			if (spr.ID == curSelected)
+			{
+				spr.animation.play('selected');
+			}
+
+			spr.updateHitbox();
+		});
+	}
+
+	override function beatHit()
+	{
+		super.beatHit();
+
+		logoBl.animation.play('bump', true);
+	}
+}
